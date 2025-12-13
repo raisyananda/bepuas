5c66555f46876de18dc8af8fe73c892536ee6572
raisyananda <raisyaadrelananda@gmail.com>
2025-12-13 23:34:11 +0700

    add main.go

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"bepuas/config"
+	"bepuas/database"
+	"bepuas/route"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	config.LoadEnv()
+
+	// PostgreSQL (Auth)
+	pg := database.ConnectPostgres()
+
+	// MongoDB (Achievement)
+	mongo := database.ConnectMongoDB()
+
+	cfg := config.NewConfig(pg, mongo)
+
+	app := fiber.New()
+	app.Use(logger.New())
+
+	// Routes
+	route.AuthRoute(app, cfg.AuthService)
+	route.PrestasiRoute(app, cfg.AchievementService)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Println("Server berjalan di http://localhost:" + port)
+	log.Fatal(app.Listen(":" + port))
+}
