cc83da2f4dbf0c65b2540cd3581150de6d2a8992
raisyananda <raisyaadrelananda@gmail.com>
2025-12-14 17:31:12 +0700

    update app/model/achievements.go

--- app/model/achievements.go
+++ app/model/achievements.go
@@ -6,13 +6,16 @@ import (
 )
 
 type Achievement struct {
-	ID         primitive.ObjectID     `bson:"_id,omitempty" json:"id,omitempty"`
-	StudentID  string                 `bson:"student_id" json:"student_id"`
-	Title      string                 `bson:"title" json:"title"`
-	AType      string                 `bson:"achievementType" json:"achievementType"`
-	Dscription string                 `bson:"description" json:"description"`
-	Details    map[string]interface{} `bson:"details" json:"details"`
-	Tags       []string               `bson:"tags" json:"tags"`
-	CreatedAt  time.Time              `bson:"created_at" json:"created_at"`
-	UpdatedAt  time.Time              `bson:"updated_at" json:"updated_at"`
+	ID              primitive.ObjectID     `bson:"_id,omitempty" json:"id,omitempty"`
+	StudentID       string                 `bson:"student_id" json:"student_id"`
+	Title           string                 `bson:"title" json:"title"`
+	AchievementType string                 `bson:"achievement_type" json:"achievement_type"`
+	Description     string                 `bson:"description" json:"description"`
+	Details         map[string]interface{} `bson:"details" json:"details"`
+	Attachments     string                 `bson:"document_url" json:"document_url"`
+	Tags            []string               `bson:"tags" json:"tags"`
+	Status          string                 `bson:"status" json:"status"`
+	IsDeleted       bool                   `bson:"is_deleted" json:"-"`
+	CreatedAt       time.Time              `bson:"created_at" json:"created_at"`
+	UpdatedAt       time.Time              `bson:"updated_at" json:"updated_at"`
 }
